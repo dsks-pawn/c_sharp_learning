09d475c4dd919f7a7a2e70ad286ce706276983a7
unknown <anhthanh1996vp@gmail.com>
2019-06-27 18:16:39 +0700

    first

---
+++ lesson1Start/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lesson1Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Console.Write();
+            // Console.Write("Hello World!");
+            // Console.WriteLine(10);
+            // Console.Write(10.9f);
+            // Console.Write(Environment.NewLine);
+            // Console.Write(true);
+
+            // Console.WriteLine(Console.ReadLine());
+            // Console.ReadKey(true);
+            #endregion
+
+
+            #region Console.Write();
+            float c = 102.2f;
+            bool e = true;
+            long g = 222;
+            int? v = null;
+            string z = null;
+            char q = 'g';
+
+            byte bienByte = 3;
+            long bienLong = 9;
+            bienLong = bienByte;
+            #endregion
+        }
+    }
+}
